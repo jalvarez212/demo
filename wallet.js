8aeb0de84142ed6d7ee5a0eda0bc86ec2e07346c
john alvarez <johncarlosalvarez1@gmail.com>
2023-12-04 18:21:43 -0500

    wallet.js

--- wallet.js
+++ wallet.js
@@ -1,4 +1,4 @@
-import {ethers} from 'ethers'
+const ethers = require('ethers');
 const wallet = ethers.Wallet.createRandom()
 console.log('address:', wallet.address)
 console.log('mnemonic:', wallet.mnemonic.phrase)
